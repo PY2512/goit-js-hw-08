6db22badca4b0f83d7c8d9e16f9bef5477d833aa
PY2512 <58562352+PY2512@users.noreply.github.com>
2022-02-09 22:30:50 +0200

    commit dz task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galeryBox = document.querySelector('.gallery');
+
+const geleryMarkup = galleryItems.map((item) => {
+    return `
+    <li>
+    <a class="gallery__item" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+        />
+    </a>
+    </li>
+    `
+}).join('');
+
+galeryBox.innerHTML = geleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
