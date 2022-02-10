9ea9aaebd57dbf7aa6c03e78302e005ee6c71d9f
PY2512 <58562352+PY2512@users.noreply.github.com>
2022-02-10 16:09:09 +0200

    dz 8 task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+
+};
+const formData = {
+    email: '',
+    message: '',
+};
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+onStorageData();
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+};
+
+function onFormSubmit(event) {
+    if (formData)
+        console.log(formData);
+    event.preventDefault();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    event.currentTarget.reset();
+};
+
+function onStorageData() {
+    const savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    const savedDataParse = JSON.parse(savedData);
+    if (savedDataParse) {
+
+        returnDataInput(savedDataParse);
+    }
+}
+
+function returnDataInput(data) {
+    formData.email = data.email;
+    formData.message = data.message;
+    refs.form.elements.email.value = formData.email;
+    refs.form.elements.message.value = formData.message;
+}
